e62ec2cd9cd000e4896829bd402faf5c066723b6
kamimura <kamimura@macbook-pro.local>
2014-08-08 20:08:08 +0900

    Initial Commit

---
+++ calendar/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  calendar
+//
+//  Created by kamimura on 8/8/14.
+//  Copyright (c) 2014 kamimura. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
